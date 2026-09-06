ef696d212c8a236791320eee314cccc195e8ba56
appieabaghough-ctr <appieabaghough@gmail.com>
2026-09-06 10:52:03 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/stop-de-mrsa",
+  assetPrefix: "/stop-de-mrsa/",
 };
 
 export default nextConfig;
